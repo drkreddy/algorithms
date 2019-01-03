0dec80e9dc42a0552a1ce9410879200d90945303
Donapati Ravindra Kumar Reddy <util4ravi@gmail.com>
2019-01-03 12:12:59 +0530

    with storage approach failed

--- src/main/java/g4g/greedy/MinimizeTowerHeght.java
+++ src/main/java/g4g/greedy/MinimizeTowerHeght.java
@@ -20,50 +20,30 @@ public class MinimizeTowerHeght {
     }
 
     private static int minimize(int[] ar, int k) {
-        int prev=ar[0];
-        int max = ar[0]+k;
-        int min = ar[0]>=k?ar[0]-k:max;
-        int current;
 
-        for (int i = 1; i < ar.length; i++) {
-            current=ar[i];
-            if(current<k){
-                current=current+k;
-            }else
-            if(min>current){
-                current=current+k;
-            }else if(max < current){
-                current=current-k;
-            }else{
-                int localmax=current+k;
-                int localMin=current-k;
+        int [] upper = new int[ar.length];
+        int [] lower = new int[ar.length];
+        int [] upperMindist = new int[ar.length];
+        int [] lowerMindist = new int[ar.length];
 
-                if((localMin >=min && localMin<=max)
-                || (localmax>=min && localmax<=max)){
-                    continue;
-                }
-                int upperdif = max-localmax;
-                int lowerdiff = min-localMin;
-                upperdif=upperdif<0?upperdif*-1:upperdif;
-                lowerdiff=lowerdiff<0?lowerdiff*-1:lowerdiff;
-
-                if(lowerdiff<upperdif) {
-                    current = localMin;
-                }else {
-                    current= localmax;
-                }
+        for(int i=0;i<ar.length;i++){
+            upper[i]=ar[i]+k;
+            if(ar[i]>k){
+                lower[i]=ar[i]-k;
+            }else {
+                lower[i]=upper[i];
             }
-
-
-           if(min>current){
-               min=current;
-           }
-           if(max<current){
-               max=current;
-           }
-
-
         }
-        return max-min;
+        upperMindist[0]=0;
+        lowerMindist[0]=0;
+        for(int i=1;i<ar.length;i++){
+            upperMindist[i]=Math.min(mod(upper[i]-upper[i-1])+upperMindist[i-1],mod(upper[i]-lower[i-1])+lowerMindist[i-1]);
+            lowerMindist[i]=Math.min(mod(lower[i]-upper[i-1])+upperMindist[i-1],mod(lower[i]-lower[i-1])+lowerMindist[i-1]);
+        }
+        return Math.min(upperMindist[ar.length-1],lowerMindist[ar.length-1]);
+    }
+    private static int mod(int a){
+        return a<0?-1*a:a;
     }
 }
+
