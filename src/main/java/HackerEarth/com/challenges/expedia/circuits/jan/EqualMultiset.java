8d3561617c834f4323fbe2d0e0bb07445db80bb1
Donapati Ravindra Kumar Reddy <util4ravi@gmail.com>
2019-02-20 08:59:53 +0530

    problems

--- src/main/java/HackerEarth/com/challenges/expedia/circuits/jan/EqualMultiset.java
+++ src/main/java/HackerEarth/com/challenges/expedia/circuits/jan/EqualMultiset.java
@@ -6,40 +6,50 @@ public class EqualMultiset {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Long> a = new ArrayList<>();
-        List<Long> b = new ArrayList<>();
-//        long[]a = new long[n];
-//        long[]b = new long[n];
+        long[]a = new long[n];
+        long[]b = new long[n];
         for(int i=0;i<n;i++){
-//            a[i]=sc.nextLong();
-            a.add(sc.nextLong());
+            a[i]=sc.nextLong();
+//            a.add(sc.nextLong());
         }
         for(int i=0;i<n;i++){
-//            b[i]=sc.nextLong();
-            b.add(sc.nextLong());
+            b[i]=sc.nextLong();
+//            b.add(sc.nextLong());
 
 
         }
-        for(int i=0;i<n;i++){
-            if(b.remove(a.get(i))){
-                a.remove(i);
-            }
-        }
-        Collections.sort(a);
-        Collections.sort(b);
+//        Collections.sort(a);
+//        Collections.sort(b);
+        Arrays.sort(a);
+        Arrays.sort(b);
+        long count=0;
+        long l=1000000007;
+//        long big=9223372036854775807l;
+//        long one=big-1;
+//        long two =big-2;
+//        long three =big-3;
+//        long onel=one%l;
+//        long twol=two%l;
+//        long threel=three%l;
+//        long sum=onel+twol+threel;
+//        long suml=sum%l;
+//
+//        System.out.println("one is "+one);
+//        System.out.println("two is "+two);
+//        System.out.println("three is "+three);
+//        System.out.println("one % l "+onel);
+//        System.out.println("two % l "+twol);
+//        System.out.println("three % l "+threel);
+//
+//        System.out.println("sum is "+sum);
+//        System.out.println("suml is "+suml);
 
-//        Arrays.sort(b);
-        double count=0;
-
-        for(int i=0;i<a.size();i++){
-//            long a1=a[i];
-//            long b1=b[i];
-            long a1=a.get(i);
-            long b1=b.get(i);
-            count+=Math.abs(a1-b1);
+        for(int i=0;i<a.length;i++){
+            long a1=a[i];
+            long b1=b[i];
+            count=(count+(Math.abs(a1-b1)%l));
         }
-        double result =count%(Math.pow(10,9)+7);
-        String s= ""+result;
-        System.out.println(s.split("\\.")[0]);
+        long result =count%l;
+        System.out.println(result);
     }
 }
--- src/main/java/HackerEarth/com/challenges/expedia/circuits/jan/Subarrays.java
+++ src/main/java/HackerEarth/com/challenges/expedia/circuits/jan/Subarrays.java
@@ -6,18 +6,32 @@ public class Subarrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        double sum=0;
+        int[]a=new int[n];
         for(int i=0;i<n;i++){
-            int k=sc.nextInt();
-            if(k>0){
-                sum+=k;
-            }
+            a[i]=sc.nextInt();
         }
 
-        if(sum>0){
-            System.out.println(sum+" 1");
-        }else{
-            System.out.println("0 0");
+        int[][] t=new int[n][n];
+        int max=0;
+        for(int i=0;i<n;i++){
+            t[i][i]=a[i];
+            if(a[i]>max){
+                max=a[i];
+            }
         }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                t[i][j]=t[i][j-1]+a[j];
+                if(t[i][j]>max){
+                    max = t[i][j];
+                }
+            }
+        }
+        if(max==0){
+            System.out.println("0 0");
+        }else {
+            System.out.println(max+" 1");
+        }
+
     }
 }
